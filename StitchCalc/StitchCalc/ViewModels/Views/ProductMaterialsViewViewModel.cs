c59a8dba50fcfb15052c45947fbbc9e467875441
Jon Stødle <jonstodle@outlook.com>
2016-07-13 10:56:01 +0200

    Add editing and deleting for product materials

--- StitchCalc/StitchCalc/ViewModels/Views/ProductMaterialsViewViewModel.cs
+++ StitchCalc/StitchCalc/ViewModels/Views/ProductMaterialsViewViewModel.cs
@@ -5,6 +5,7 @@ using StitchCalc.ViewModels.Models;
 using StitchCalc.Views;
 using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,21 +14,37 @@ namespace StitchCalc.ViewModels.Views
 	public class ProductMaterialsViewViewModel : ViewModelBase, INavigable
 	{
 		readonly ReactiveCommand<object> navigateToMaterialFormView;
+		readonly ReactiveCommand<object> edit;
 		ProductViewModel product;
+		object selectedProductMaterial;
 
 		public ProductMaterialsViewViewModel()
 		{
 			navigateToMaterialFormView = ReactiveCommand.Create();
 			navigateToMaterialFormView
 				.Subscribe(_ => NavigationService.Current.NavigateTo<ProductMaterialFormView>(product.Model.Id));
+
+			edit = ReactiveCommand.Create();
+			edit
+				.Select(_ => selectedProductMaterial)
+				.Cast<ProductMaterialViewModel>()
+				.Subscribe(item => NavigationService.Current.NavigateTo<ProductMaterialFormView>(item.Model.Id));
 		}
 
 		public ReactiveCommand<object> NavigateToProductMaterialFormView => navigateToMaterialFormView;
+
+		public ReactiveCommand<object> Edit => edit;
 
 		public ProductViewModel Product
 		{
 			get { return product; }
 			set { this.RaiseAndSetIfChanged(ref product, value); }
+		}
+
+		public object SelectedProductMaterial
+		{
+			get { return selectedProductMaterial; }
+			set { this.RaiseAndSetIfChanged(ref selectedProductMaterial, value); }
 		}
 
 		public Task OnNavigatedTo(object parameter, NavigationDirection direction)
--- StitchCalc/StitchCalc/Views/ProductMaterialsView.xaml.cs
+++ StitchCalc/StitchCalc/Views/ProductMaterialsView.xaml.cs
@@ -3,6 +3,7 @@ using StitchCalc.ViewModels.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +17,13 @@ namespace StitchCalc.Views
 		{
 			InitializeComponent ();
 
+			this.BindCommand(ViewModel, vm => vm.NavigateToProductMaterialFormView, v => v.AddProductMaterialsToolbarItem);
 			this.OneWayBind(ViewModel, vm => vm.Product.Materials, v => v.MaterialsListView.ItemsSource);
 			this.OneWayBind(ViewModel, vm => vm.Product.MaterialsPrice, v => v.SumLabel.Text);
-			this.BindCommand(ViewModel, vm => vm.NavigateToProductMaterialFormView, v => v.AddProductMaterialsToolbarItem);
+			this.Bind(ViewModel, vm => vm.SelectedProductMaterial, v => v.MaterialsListView.SelectedItem);
+
+			Observable.FromEventPattern(MaterialsListView, nameof(ListView.ItemTapped))
+				.InvokeCommand(ViewModel, x => x.Edit);
 		}
 
 		public ProductMaterialsViewViewModel ViewModel
