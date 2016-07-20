32dcabd70d2ea09e246afd4b42b2ecb5ea188d16
Jon Stødle <jonstodle@outlook.com>
2016-07-20 15:31:14 +0200

    Tweak visibility of unactive charge

--- StitchCalc/StitchCalc/Views/ProductSummaryView.xaml.cs
+++ StitchCalc/StitchCalc/Views/ProductSummaryView.xaml.cs
@@ -19,7 +19,7 @@ namespace StitchCalc.Views
 			this.BindCommand(ViewModel, vm => vm.Edit, v => v.EditProductToolbarItem);
 
 			this.BindCommand(ViewModel, vm => vm.Model.ToggleChargeForMaterials, v => v.MaterialsStackLayoutTapGestureRecognizer, nameof(TapGestureRecognizer.Tapped));
-			this.OneWayBind(ViewModel, vm => vm.Model.ChargeForMaterials, v => v.MaterialsStackLayout.Opacity, x=> x ? 1 : .5);
+			this.OneWayBind(ViewModel, vm => vm.Model.ChargeForMaterials, v => v.MaterialsStackLayout.Opacity, x=> x ? 1 : .2);
 			this.OneWayBind(ViewModel, vm => vm.Model.MaterialsPrice, v => v.MaterialsCostLabel.Text, x => x.ToString("N2"));
 			this.OneWayBind(ViewModel, vm => vm.Model.IsMaterialsPriceMultiplied, v => v.MaterialsMultiplierLabel.IsVisible);
 			this.OneWayBind(ViewModel, vm => vm.Model.Multiplier, v => v.MaterialsMultiplierLabel.Text, x => $"x{x}");
