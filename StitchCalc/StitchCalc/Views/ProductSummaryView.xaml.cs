f77520afe8a5d9c4964b1727cb9d2599b32f5a64
Jon Stødle <jonstodle@outlook.com>
2016-07-20 16:11:44 +0200

    Switch to action sheet

--- StitchCalc/StitchCalc/Views/ProductSummaryView.xaml.cs
+++ StitchCalc/StitchCalc/Views/ProductSummaryView.xaml.cs
@@ -3,6 +3,7 @@ using StitchCalc.ViewModels.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,20 +19,46 @@ namespace StitchCalc.Views
 
 			this.BindCommand(ViewModel, vm => vm.Edit, v => v.EditProductToolbarItem);
 
-			this.BindCommand(ViewModel, vm => vm.Model.ToggleChargeForMaterials, v => v.MaterialsStackLayoutTapGestureRecognizer, nameof(TapGestureRecognizer.Tapped));
 			this.OneWayBind(ViewModel, vm => vm.Model.ChargeForMaterials, v => v.MaterialsStackLayout.Opacity, x=> x ? 1 : .2);
 			this.OneWayBind(ViewModel, vm => vm.Model.MaterialsPrice, v => v.MaterialsCostLabel.Text, x => x.ToString("N2"));
 			this.OneWayBind(ViewModel, vm => vm.Model.IsMaterialsPriceMultiplied, v => v.MaterialsMultiplierLabel.IsVisible);
-			this.OneWayBind(ViewModel, vm => vm.Model.Multiplier, v => v.MaterialsMultiplierLabel.Text, x => $"x{x}");
+			this.OneWayBind(ViewModel, vm => vm.Model.MaterialsMultiplier, v => v.MaterialsMultiplierLabel.Text, x => $"x{x}");
 
-			this.BindCommand(ViewModel, vm => vm.Model.ToggleChargeForWork, v => v.WorkStackLayoutTapGestureRecognizer, nameof(TapGestureRecognizer.Tapped));
 			this.OneWayBind(ViewModel, vm => vm.Model.ChargeForWork, v => v.WorkStackLayout.Opacity, x => x ? 1: .5);
 			this.OneWayBind(ViewModel, vm => vm.Model.WorkPrice, v => v.WorkCostLabel.Text, x => x.ToString("N2"));
 			this.OneWayBind(ViewModel, vm => vm.Model.IsWorkPriceMultiplied, v => v.WorkMultiplierLabel.IsVisible);
-			this.OneWayBind(ViewModel, vm => vm.Model.Multiplier, v => v.WorkMultiplierLabel.Text, x=> $"x{x}");
+			this.OneWayBind(ViewModel, vm => vm.Model.WorkMultiplier, v => v.WorkMultiplierLabel.Text, x=> $"x{x}");
 
 			this.OneWayBind(ViewModel, vm => vm.Model.TotalPrice, v => v.SumCostLabel.Text, x => x.ToString("N2"));
+
+			Observable
+				.FromEventPattern(MaterialsStackLayoutTapGestureRecognizer, nameof(TapGestureRecognizer.Tapped))
+				.Subscribe(_ => ShowActionSheet("Materials", ChargeReasons.Materials));
+
+			Observable
+				.FromEventPattern(WorkStackLayoutTapGestureRecognizer, nameof(TapGestureRecognizer.Tapped))
+				.Subscribe(_ => ShowActionSheet("Work", ChargeReasons.Work));
 		}
+
+		enum ChargeReasons { Materials, Work }
+		async void ShowActionSheet(string title, ChargeReasons reason)
+		{
+			var actions = new string[] { "Cancel", "Toggle", "Multiply" };
+
+			var result = await DisplayActionSheet("", actions[0], null, actions[1], actions[2]);
+
+			if (result == actions[1])
+			{
+				if (reason == ChargeReasons.Materials) { ViewModel.Model.ToggleChargeForMaterials.Execute(null); }
+				else if(reason == ChargeReasons.Work) { ViewModel.Model.ToggleChargeForWork.Execute(null); }
+			}
+			else if (result == actions[2])
+			{
+				
+			}
+		}
+
+
 
 		public ProductSummaryViewViewModel ViewModel
 		{
