9840c9fbbdf5b0b6216f27d2c8ee431ce637e1e1
Jon Stødle <jonstodle@outlook.com>
2016-07-27 13:43:34 +0200

    Prevent touching file too often

--- StitchCalc/StitchCalc/Services/DataServices/DataService.cs
+++ StitchCalc/StitchCalc/Services/DataServices/DataService.cs
@@ -27,6 +27,7 @@ namespace StitchCalc.Services.DataServices
 				workUnits.Changed,
 				customProperties.Changed,
 				productMaterials.Changed)
+				.Throttle(TimeSpan.FromSeconds(1))
 				.Subscribe(_ => SaveDataToDisk());
 #pragma warning restore 4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 		}
