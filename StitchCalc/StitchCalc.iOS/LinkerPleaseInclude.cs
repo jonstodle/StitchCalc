4f3316e83410a94a4235e7b8f8340de66f9c79b2
Jon Stødle <jonstodle@outlook.com>
2016-09-12 18:48:48 +0200

    Fix crash when running on device

---
+++ StitchCalc/StitchCalc.iOS/LinkerPleaseInclude.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StitchCalc.iOS
+{
+	public class LinkerPleaseInclude
+	{
+		public void Include()
+		{
+			var x = new System.ComponentModel.ReferenceConverter(typeof(void));
+		}
+	}
+}
+
