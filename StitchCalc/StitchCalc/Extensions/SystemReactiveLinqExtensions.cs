29792f3662d59931fc1ad3cd72e1af8e224a025d
Unknown <jonstodle@outlook.com>
2016-09-30 09:44:44 +0200

    Add Rx extension

---
+++ StitchCalc/StitchCalc/Extensions/SystemReactiveLinqExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace System.Reactive.Linq
+{
+	public static class SystemReactiveLinqExtensions
+	{
+		public static IObservable<T> WhereNotNull<T>(this IObservable<T> source) where T : class => source.Where(x => x != null);
+	}
+}
