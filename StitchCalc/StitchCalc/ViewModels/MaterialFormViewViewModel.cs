c5ab82c1309681d01751977d21bd60629bf63d80
Jon Stødle <jonstodle@outlook.com>
2017-02-17 23:27:22 +0100

    Move MaterialFormViewViewModel to Realm

--- StitchCalc/StitchCalc/ViewModels/MaterialFormViewViewModel.cs
+++ StitchCalc/StitchCalc/ViewModels/MaterialFormViewViewModel.cs
@@ -8,117 +8,113 @@ using System.Threading.Tasks;
 
 namespace StitchCalc.ViewModels
 {
-	public class MaterialFormViewViewModel : ViewModelBase, INavigable
-	{
-		readonly ReactiveCommand<Unit, Unit> save;
-		string pageTitle;
-		string name;
-		string width;
-		string price;
-		string notes;
-		Material material;
+    public class MaterialFormViewViewModel : ViewModelBase, INavigable
+    {
+        public MaterialFormViewViewModel()
+        {
+            _save = ReactiveCommand.CreateFromTask(
+                () => SaveImpl(),
+                this.WhenAnyValue(a => a.Name, b => b.Width, c => c.Price, (a, b, c) =>
+                {
+                    double amnt, prc = default(double);
+                    return !string.IsNullOrWhiteSpace(a)
+                    && !string.IsNullOrWhiteSpace(b)
+                    && !string.IsNullOrWhiteSpace(b)
+                    && double.TryParse(b, out amnt)
+                    && double.TryParse(c, out prc)
+                    && amnt > 0
+                    && prc > 0;
+                }));
+        }
 
-		public MaterialFormViewViewModel()
-		{
-			save = ReactiveCommand.CreateFromTask(
-				() => SaveImpl(),
-				this.WhenAnyValue(a => a.Name, b => b.Width, c => c.Price, (a, b, c) =>
-				{
-					double amnt, prc = default(double);
-					return !string.IsNullOrWhiteSpace(a)
-					&& !string.IsNullOrWhiteSpace(b)
-					&& !string.IsNullOrWhiteSpace(b)
-					&& double.TryParse(b, out amnt)
-					&& double.TryParse(c, out prc)
-					&& amnt > 0
-					&& prc > 0;
-				}));
-		}
+        public ReactiveCommand Save => _save;
 
-		public ReactiveCommand Save => save;
+        public string PageTitle
+        {
+            get { return _pageTitle; }
+            set { this.RaiseAndSetIfChanged(ref _pageTitle, value); }
+        }
 
-		public string PageTitle
-		{
-			get { return pageTitle; }
-			set { this.RaiseAndSetIfChanged(ref pageTitle, value); }
-		}
+        public string Name
+        {
+            get { return _name; }
+            set { this.RaiseAndSetIfChanged(ref _name, value); }
+        }
 
-		public string Name
-		{
-			get { return name; }
-			set { this.RaiseAndSetIfChanged(ref name, value); }
-		}
+        public string Width
+        {
+            get { return _width; }
+            set { this.RaiseAndSetIfChanged(ref _width, value); }
+        }
 
-		public string Width
-		{
-			get { return width; }
-			set { this.RaiseAndSetIfChanged(ref width, value); }
-		}
+        public string Price
+        {
+            get { return _price; }
+            set { this.RaiseAndSetIfChanged(ref _price, value); }
+        }
 
-		public string Price
-		{
-			get { return price; }
-			set { this.RaiseAndSetIfChanged(ref price, value); }
-		}
+        public string Notes
+        {
+            get { return _notes; }
+            set { this.RaiseAndSetIfChanged(ref _notes, value); }
+        }
 
-		public string Notes
-		{
-			get { return notes; }
-			set { this.RaiseAndSetIfChanged(ref notes, value); }
-		}
-
-		public Material Material
-		{
-			get { return material; }
-			set { this.RaiseAndSetIfChanged(ref material, value); }
-		}
+        public Material Material
+        {
+            get { return _material; }
+            set { this.RaiseAndSetIfChanged(ref _material, value); }
+        }
 
 
 
-		async Task SaveImpl()
-		{
-			SaveMaterial();
+        public Task OnNavigatedTo(object parameter, NavigationDirection direction)
+        {
+            if (parameter is Guid)
+            {
+                Material = DBService.Get<Material>(x => x.Id == (Guid)parameter);
 
-			await NavigationService.Current.GoBack();
-		}
+                PageTitle = "Edit Material";
+                Name = _material.Name;
+                Width = _material.Width.ToString();
+                Price = _material.Price.ToString();
+                Notes = _material.Notes;
+            }
+            else
+            {
+                PageTitle = "Add Material";
+            }
 
-		Material SaveMaterial()
-		{
-			var mtrl = new Material
-			{
-				Id = material?.Model.Id ?? default(Guid),
-				Name = Name,
-				Width = double.Parse(Width),
-				Price = double.Parse(Price),
-				Notes = notes
-			};
+            return Task.CompletedTask;
+        }
 
-			if (Material == null) { return DataService.Current.Add(mtrl); }
-			else { return DataService.Current.Update(mtrl); }
-		}
+        public Task OnNavigatingFrom() => Task.CompletedTask;
 
 
 
-		public Task OnNavigatedTo(object parameter, NavigationDirection direction)
-		{
-			if (parameter is Guid)
-			{
-				Material = DataService.Current.GetMaterial((Guid)parameter);
+        private async Task SaveImpl()
+        {
+            var material = new Material
+            {
+                Name = Name,
+                Width = double.Parse(Width),
+                Price = double.Parse(Price),
+                Notes = _notes
+            };
+            if (_material != null) material.Id = _material.Id;
 
-				PageTitle = "Edit Material";
-				Name = material.Model.Name;
-				Width = material.Model.Width.ToString();
-				Price = material.Model.Price.ToString();
-		Notes = material.Model.Notes;
-			}
-			else
-			{
-				PageTitle = "Add Material";
-			}
+            DBService.Write(realm => realm.Add(material, true));
 
-			return Task.CompletedTask;
-		}
+            await NavigationService.Current.GoBack();
+        }
 
-		public Task OnNavigatingFrom() => Task.CompletedTask;
-	}
+
+
+        private readonly ReactiveCommand<Unit, Unit> _save;
+        private string _pageTitle;
+        private string _name;
+        private string _width;
+        private string _price;
+        private string _notes;
+        private Material _material;
+    }
 }
