2d0d4c6f8fa6c27069fc6648c8998a5068e46875
Jon Stødle <jonstodle@outlook.com>
2017-02-17 23:05:54 +0100

    Add HasValue string extension

--- StitchCalc/StitchCalc/Extensions/SystemExtensions.cs
+++ StitchCalc/StitchCalc/Extensions/SystemExtensions.cs
@@ -7,5 +7,7 @@
 			double d;
 			return double.TryParse(s, out d);
 		}
+
+        public static bool HasValue(this string source, int minimumLength = 1) => string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
 	}
 }
