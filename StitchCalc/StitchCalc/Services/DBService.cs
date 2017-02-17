35682739ac3732ea4300bd240709b4295ee5f406
Jon Stødle <jonstodle@outlook.com>
2017-02-17 23:06:08 +0100

    Add method for getting record with filter

--- StitchCalc/StitchCalc/Services/DBService.cs
+++ StitchCalc/StitchCalc/Services/DBService.cs
@@ -25,6 +25,13 @@ namespace StitchCalc
 			else return _realm.All<T>().OrderByDescending(orderFunction).AsRealmCollection();
 		}
 
+        public static IRealmCollection<T> Get<T, TSortKey>(Expression<Func<T, bool>> filterFunction, Expression<Func<T, TSortKey>> orderFunction = null, bool sortAscending = true) where T: RealmObject
+        {
+            var records = _realm.All<T>().Where(filterFunction);
+            if (orderFunction != null) records = sortAscending ? records.OrderBy(orderFunction) : records.OrderByDescending(orderFunction);
+            return records.AsRealmCollection();
+        }
+
 
 
 		private static Realm _realm;
