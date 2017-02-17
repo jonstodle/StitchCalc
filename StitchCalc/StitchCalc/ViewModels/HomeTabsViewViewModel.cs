8eb5c5216e9cfd4b1e3283e87912fb03b23d50d7
Jon Stødle <jonstodle@outlook.com>
2017-02-17 23:06:36 +0100

    Simplify HomeTabsView and VM

--- StitchCalc/StitchCalc/ViewModels/HomeTabsViewViewModel.cs
+++ StitchCalc/StitchCalc/ViewModels/HomeTabsViewViewModel.cs
@@ -10,36 +10,9 @@ namespace StitchCalc.ViewModels
 {
 	public class HomeTabsViewViewModel : ViewModelBase, INavigable
 	{
-		readonly ReactiveList<Page> pages;
+		public HomeTabsViewViewModel() { }
 
-		public HomeTabsViewViewModel()
-		{
-			pages = new ReactiveList<Page>();
-			pages.Add(new HomeView());
-			pages.Add(new MaterialsView());
-			pages.Add(new SettingsView());
-
-			pages
-				.Changed
-				.Throttle(TimeSpan.FromMilliseconds(10))
-				.Subscribe(_ => NavigateToPages(null, NavigationDirection.Forwards));
-		}
-
-		public ReactiveList<Page> Pages => pages;
-
-		async void NavigateToPages(object parameter, NavigationDirection direction)
-		{
-			foreach (var page in pages)
-			{
-				await((page as IViewFor).ViewModel as INavigable).OnNavigatedTo(parameter, direction);
-			}
-		}
-
-		public Task OnNavigatedTo(object parameter, NavigationDirection direction)
-		{
-			NavigateToPages(parameter, direction);
-			return Task.CompletedTask;
-		}
+        public Task OnNavigatedTo(object parameter, NavigationDirection direction) => Task.CompletedTask;
 
 		public Task OnNavigatingFrom() => Task.CompletedTask;
 	}
--- StitchCalc/StitchCalc/Views/HomeTabsView.cs
+++ StitchCalc/StitchCalc/Views/HomeTabsView.cs
@@ -1,52 +1,30 @@
-﻿using ReactiveUI;
-using System;
-using System.Linq;
-using System.Reactive.Linq;
+﻿using ReactiveUI;using System;using System.Linq;using System.Reactive.Linq;using Xamarin.Forms;using System.Reactive.Disposables;using System.Reactive;using StitchCalc.ViewModels;namespace StitchCalc.Views{
+    public class HomeTabsView : TabbedPage, IViewFor<HomeTabsViewViewModel>, ICanActivate
+    {
+        public HomeTabsView()
+        {
+            ViewModel = new HomeTabsViewViewModel();
 
-using Xamarin.Forms;
-using System.Reactive.Disposables;
-using System.Reactive;
-using StitchCalc.ViewModels;
+            Title = "StitchCalc";            Children.Add(new HomeView());
+            Children.Add(new MaterialsView());
+            Children.Add(new SettingsView());
+        }
 
-namespace StitchCalc.Views
-{
-	public class HomeTabsView : TabbedPage, IViewFor<HomeTabsViewViewModel>, ICanActivate
-	{
-		public HomeTabsView()
-		{
-			ViewModel = new HomeTabsViewViewModel();
+        public HomeTabsViewViewModel ViewModel
+        {
+            get { return (HomeTabsViewViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
 
-			Title = "StitchCalc";
+        public static readonly BindableProperty ViewModelProperty = BindableProperty.Create(nameof(ViewModel), typeof(HomeTabsViewViewModel), typeof(HomeTabsView), null);
 
-			ViewModel
-				.Pages
-				.Changed
-				.Throttle(TimeSpan.FromMilliseconds(10))
-				.Select(_ => ViewModel.Pages)
-				.StartWith(ViewModel.Pages)
-				.Subscribe(pages =>
-				{
-					Children.Clear();
-					foreach (var page in pages) { Children.Add(page); }
-				});
-		}
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (HomeTabsViewViewModel)value; }
+        }
 
-		public HomeTabsViewViewModel ViewModel
-		{
-			get { return (HomeTabsViewViewModel)GetValue(ViewModelProperty); }
-			set { SetValue(ViewModelProperty, value); }
-		}
+        public IObservable<Unit> Activated => Observable.FromEventPattern(this, nameof(this.Appearing)).ToSignal();
 
-		public static readonly BindableProperty ViewModelProperty = BindableProperty.Create(nameof(ViewModel), typeof(HomeTabsViewViewModel), typeof(HomeTabsView), null);
-
-		object IViewFor.ViewModel
-		{
-			get { return ViewModel; }
-			set { ViewModel = (HomeTabsViewViewModel)value; }
-		}
-
-		public IObservable<Unit> Activated => Observable.FromEventPattern(this, nameof(this.Appearing)).ToSignal();
-
-		public IObservable<Unit> Deactivated => Observable.FromEventPattern(this, nameof(this.Disappearing)).ToSignal();
-	}
-}
+        public IObservable<Unit> Deactivated => Observable.FromEventPattern(this, nameof(this.Disappearing)).ToSignal();
+    }}
