e410f328bb938cf78a3028529720d539c3d6c32c
Unknown <jonstodle@outlook.com>
2017-02-17 20:31:13 +0100

    Add Get methods

--- StitchCalc/StitchCalc/Services/DBService.cs
+++ StitchCalc/StitchCalc/Services/DBService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Expressions;
 using Realms;
 using StitchCalc.Models;
 
@@ -14,7 +15,15 @@ namespace StitchCalc
 
 
 
-		public static Realm DB => _realm;
+		public static void Write(Action<Realm> writeActions) => _realm.Write(() => writeActions(_realm));
+
+		public static IRealmCollection<T> Get<T>() where T : RealmObject => _realm.All<T>().AsRealmCollection();
+
+		public static IRealmCollection<T> Get<T, TKey>(Expression<Func<T, TKey>> orderFunction, bool sortAscending = true) where T : RealmObject
+		{
+			if (sortAscending) return _realm.All<T>().OrderBy(orderFunction).AsRealmCollection();
+			else return _realm.All<T>().OrderByDescending(orderFunction).AsRealmCollection();
+		}
 
 
 
