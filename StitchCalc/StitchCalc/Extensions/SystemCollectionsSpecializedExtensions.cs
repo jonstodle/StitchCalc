a5d9ad3c2db78f7a96e6d40e796b432ad68e5f5f
Jon Stødle <jonstodle@outlook.com>
2017-02-17 23:05:40 +0100

    Add extension to easily watch for collection changes

---
+++ StitchCalc/StitchCalc/Extensions/SystemCollectionsSpecializedExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace System.Collections.Specialized
+{
+    public static class SystemCollectionsSpecializedExtensions
+    {
+        public static IObservable<EventPattern<NotifyCollectionChangedEventArgs>> Changed(this INotifyCollectionChanged source) => Observable.FromEventPattern<NotifyCollectionChangedEventArgs>(source, nameof(source.CollectionChanged));
+    }
+}
