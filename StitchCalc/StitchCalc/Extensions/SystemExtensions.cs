461a736351a05d5964aa2e8255c3c3a5c01b83c5
Unknown <jonstodle@outlook.com>
2017-02-18 17:13:18 +0100

    Fix forgetting to flip bool when checking null string

--- StitchCalc/StitchCalc/Extensions/SystemExtensions.cs
+++ StitchCalc/StitchCalc/Extensions/SystemExtensions.cs
@@ -8,6 +8,6 @@
 			return double.TryParse(s, out d);
 		}
 
-        public static bool HasValue(this string source, int minimumLength = 1) => string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
+        public static bool HasValue(this string source, int minimumLength = 1) => !string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
 	}
 }
