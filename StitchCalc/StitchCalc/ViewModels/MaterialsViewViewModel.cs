f54220ea2b71495dd9cc3a57a7d53e0dd050b8b7
Jon Stødle <jonstodle@outlook.com>
2017-02-18 11:15:35 +0100

    Move MaterialsViewViewModel to Realm

--- StitchCalc/StitchCalc/ViewModels/MaterialsViewViewModel.cs
+++ StitchCalc/StitchCalc/ViewModels/MaterialsViewViewModel.cs
@@ -8,75 +8,72 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Realms;
 using StitchCalc.Models;
+using System.Collections.Specialized;
 
 namespace StitchCalc.ViewModels
 {
 	public class MaterialsViewViewModel : ViewModelBase, INavigable
 	{
-		readonly IRealmCollection<Material> materials;
-		readonly ObservableAsPropertyHelper<IRealmCollection<Material>> collectionView;
-		readonly ReactiveCommand<Unit, Unit> navigateToMaterialFormView;
-		readonly ReactiveCommand<Unit, Unit> edit;
-		string searchTerm;
-		object selectedMaterial;
-
 		public MaterialsViewViewModel()
 		{
-			materials = DataService.Current.GetMaterials();
+            _materials = DBService.Get<Material, string>(x => x.Name);
 
-			this
-				.WhenAnyValue(x => x.SearchTerm)
-				.Merge(materials.Changed.Select(_ => ""))
-				.Throttle(TimeSpan.FromMilliseconds(500), RxApp.TaskpoolScheduler)
-				.Select(x => CreateDerivedList(x))
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.ToProperty(this, x => x.CollectionView, out collectionView);
+			_navigateToMaterialFormView = ReactiveCommand.CreateFromTask(() => NavigationService.Current.NavigateTo<MaterialFormView>());
 
-			navigateToMaterialFormView = ReactiveCommand.CreateFromTask(() => NavigationService.Current.NavigateTo<MaterialFormView>());
+			_edit = ReactiveCommand.CreateFromTask(x => NavigationService.Current.NavigateTo<MaterialFormView>(_selectedMaterial.Id));
 
-			edit = ReactiveCommand.CreateFromTask(x => NavigationService.Current.NavigateTo<MaterialFormView>((selectedMaterial as MaterialViewModel).Model.Id));
-		}
+            _collectionView = Observable.Merge(
+                    this.WhenAnyValue(x => x.SearchTerm),
+                    _materials.Changed().Select(_ => ""))
+                .Throttle(TimeSpan.FromMilliseconds(500), RxApp.TaskpoolScheduler)
+                .Select(x => CreateFilteredList(x))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .ToProperty(this, x => x.CollectionView, out _collectionView);
+        }
 
-		public IRealmCollection<Material> Materials => materials;
 
-		public IRealmCollection<Material> CollectionView => collectionView.Value;
+
+        public ReactiveCommand NavigateToMaterialFormView => _navigateToMaterialFormView;
+
+        public ReactiveCommand Edit => _edit;
+
+        public IRealmCollection<Material> Materials => _materials;
+
+		public IRealmCollection<Material> CollectionView => _collectionView.Value;
 
 		public string SearchTerm
 		{
-			get { return searchTerm; }
-			set { this.RaiseAndSetIfChanged(ref searchTerm, value); }
+			get { return _searchTerm; }
+			set { this.RaiseAndSetIfChanged(ref _searchTerm, value); }
 		}
 
-		public object SelectedMaterial
+		public Material SelectedMaterial
 		{
-			get { return selectedMaterial; }
-			set { this.RaiseAndSetIfChanged(ref selectedMaterial, value); }
+			get { return _selectedMaterial; }
+			set { this.RaiseAndSetIfChanged(ref _selectedMaterial, value); }
 		}
 
-		public ReactiveCommand NavigateToMaterialFormView => navigateToMaterialFormView;
-
-		public ReactiveCommand Edit => edit;
 
 
+        public Task OnNavigatedTo(object parameter, NavigationDirection direction) => Task.CompletedTask;
 
-		private IRealmCollection<Material> CreateDerivedList(string searchString)
+        public Task OnNavigatingFrom() => Task.CompletedTask;
+
+
+
+        private IRealmCollection<Material> CreateFilteredList(string searchString)
 		{
-			var orderFunc = new Func<MaterialViewModel, MaterialViewModel, int>((item1, item2) => item1.Name.CompareTo(item2.Name));
-
-			if (string.IsNullOrWhiteSpace(searchString))
-			{
-				return Materials.CreateDerivedCollection(x => x, orderer: orderFunc);
-			}
-			else
-			{
-				return Materials.CreateDerivedCollection(x => x, x => x.Name.ToLowerInvariant().Contains(searchString.ToLowerInvariant()), orderFunc);
-			}
+            if (searchString.HasValue()) return _materials;
+            else return DBService.Get<Material, string>(x => x.Name.ToLowerInvariant().Contains(searchString.ToLowerInvariant()), x => x.Name);
 		}
 
 
 
-		public Task OnNavigatedTo(object parameter, NavigationDirection direction) => Task.CompletedTask;
-
-		public Task OnNavigatingFrom() => Task.CompletedTask;
-	}
+        private readonly IRealmCollection<Material> _materials;
+        private readonly ObservableAsPropertyHelper<IRealmCollection<Material>> _collectionView;
+        private readonly ReactiveCommand<Unit, Unit> _navigateToMaterialFormView;
+        private readonly ReactiveCommand<Unit, Unit> _edit;
+        private string _searchTerm;
+        private Material _selectedMaterial;
+    }
 }
