56d8d7d1eb6564ae3b5bfafaf258050d0902744b
Jon Stødle <jonstodle@outlook.com>
2017-02-27 00:29:08 +0100

    Add ProductMaterial VM

---
+++ StitchCalc/StitchCalc/ViewModels/ProductMaterialViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using StitchCalc.Models;
+using StitchCalc.Services;
+using StitchCalc.Views;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace StitchCalc.ViewModels
+{
+    public class ProductMaterialViewModel : ViewModelBase
+    {
+        public ProductMaterialViewModel(ProductMaterial productMaterial)
+        {
+            _productMaterial = productMaterial;
+
+            _edit = ReactiveCommand.CreateFromObservable(() => Observable.FromAsync(() => NavigationService.NavigateTo(new ProductMaterialFormView(new ProductMaterialFormViewModel(_productMaterial.Product, _productMaterial)))));
+        }
+
+
+
+        public ReactiveCommand Edit => _edit;
+
+        public ProductMaterial ProductMaterial => _productMaterial;
+
+
+
+        private readonly ReactiveCommand<Unit, Unit> _edit;
+        private readonly ProductMaterial _productMaterial;
+    }
+}
