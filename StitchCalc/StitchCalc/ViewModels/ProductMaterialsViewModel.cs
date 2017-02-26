e79531f35229f41d967cb22f7209d9e89fbc8070
Jon Stødle <jonstodle@outlook.com>
2017-02-27 00:39:23 +0100

    Add ProductMaterials VM

---
+++ StitchCalc/StitchCalc/ViewModels/ProductMaterialsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using ReactiveUI;
+using StitchCalc.Views;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using StitchCalc.Models;
+using Realms;
+using System.Collections.Specialized;
+using System.Linq;
+using StitchCalc.Services;
+
+namespace StitchCalc.ViewModels
+{
+	public class ProductMaterialsViewModel : ViewModelBase
+	{
+		public ProductMaterialsViewModel(Product product)
+		{
+            _product = product;
+
+			_addProductMaterial = ReactiveCommand.CreateFromObservable(() => Observable.FromAsync(() => NavigationService.NavigateTo(new ProductMaterialFormView(new ProductMaterialFormViewModel(_product)))));
+
+            _productMaterials = _product.Materials.OrderBy(x => x.Name).AsRealmCollection();
+
+            _productMaterialsView = _productMaterials.CreateDerivedCollection(x => new ProductMaterialViewModel(x));
+
+			_materialsPrice = _productMaterials
+                .CollectionChanges()
+				.Select(_ => _productMaterials.Sum(x => x.Price))
+				.ToProperty(this, x => x.MaterialsPrice);
+		}
+
+
+
+		public ReactiveCommand AddProductMaterial => _addProductMaterial;
+
+        public IRealmCollection<ProductMaterial> ProductMaterials => _productMaterials;
+
+        public IReactiveDerivedList<ProductMaterialViewModel> ProductMaterialsView => _productMaterialsView;
+
+        public Product Product => _product;
+
+        public double MaterialsPrice => _materialsPrice.Value;
+
+		public ProductMaterialViewModel SelectedProductMaterial
+		{
+			get { return _selectedProductMaterial; }
+			set { this.RaiseAndSetIfChanged(ref _selectedProductMaterial, value); }
+		}
+
+
+
+		private readonly ReactiveCommand<Unit, Unit> _addProductMaterial;
+        private readonly IRealmCollection<ProductMaterial> _productMaterials;
+        private readonly IReactiveDerivedList<ProductMaterialViewModel> _productMaterialsView;
+        private readonly Product _product;
+		private readonly ObservableAsPropertyHelper<double> _materialsPrice;
+		private ProductMaterialViewModel _selectedProductMaterial;
+	}
+}
--- StitchCalc/StitchCalc/Views/ProductMaterialsView.xaml.cs
+++ StitchCalc/StitchCalc/Views/ProductMaterialsView.xaml.cs
@@ -9,9 +9,9 @@ using StitchCalc.ViewModels;
 
 namespace StitchCalc.Views
 {
-    public partial class ProductMaterialsView : ReactiveContentPage<ProductViewModel>
+    public partial class ProductMaterialsView : ReactiveContentPage<ProductMaterialsViewModel>
     {
-        public ProductMaterialsView(ProductViewModel viewModel)
+        public ProductMaterialsView(ProductMaterialsViewModel viewModel)
         {
             InitializeComponent();
 
@@ -19,12 +19,12 @@ namespace StitchCalc.Views
 
             this.WhenActivated(disposables =>
             {
-                this.OneWayBind(ViewModel, vm => vm.Product.Materials, v => v.MaterialsListView.ItemsSource).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.ProductMaterialsView, v => v.MaterialsListView.ItemsSource).DisposeWith(disposables);
                 this.OneWayBind(ViewModel, vm => vm.MaterialsPrice, v => v.SumLabel.Text, x => x.ToString("N2")).DisposeWith(disposables);
                 this.Bind(ViewModel, vm => vm.SelectedProductMaterial, v => v.MaterialsListView.SelectedItem).DisposeWith(disposables);
 
-                this.BindCommand(ViewModel, vm => vm.NavigateToProductMaterialFormView, v => v.AddProductMaterialsToolbarItem).DisposeWith(disposables);
-                this.BindCommand(ViewModel, vm => vm.Edit, v => v.MaterialsListView, nameof(ListView.ItemTapped)).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.AddProductMaterial, v => v.AddProductMaterialsToolbarItem).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.SelectedProductMaterial.Edit, v => v.MaterialsListView, nameof(ListView.ItemTapped)).DisposeWith(disposables);
 
                 MaterialsListView.SelectedItem = null;
             });
