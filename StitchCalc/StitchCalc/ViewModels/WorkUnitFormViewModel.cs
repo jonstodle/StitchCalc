78fa6bd74338cad72951f29fb6b9968edc8de570
Jon Stødle <jonstodle@outlook.com>
2017-02-26 23:47:44 +0100

    Move WorkUnitForm to new injection model

---
+++ StitchCalc/StitchCalc/ViewModels/WorkUnitFormViewModel.cs
@@ -0,0 +1,84 @@
+﻿using ReactiveUI;
+using StitchCalc.Models;
+using StitchCalc.Services;
+using StitchCalc.Services.SettingsServices;
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace StitchCalc.ViewModels
+{
+    public class WorkUnitFormViewModel : ViewModelBase
+    {
+        public WorkUnitFormViewModel(Product product, WorkUnit workUnit = null)
+        {
+            _product = product;
+            _workUnit = workUnit;
+            Name = _workUnit?.Name;
+            Minutes = _workUnit?.Minutes.ToString();
+            Charge = (_workUnit?.Charge * 60).ToString() ?? SettingsService.Current.DefaultHourlyCharge.ToString();
+
+            _save = ReactiveCommand.CreateFromObservable(
+                () => SaveImpl(),
+                this.WhenAnyValue(x => x.Name, y => y.Minutes, z => z.Charge, (x, y, z) =>
+                !string.IsNullOrWhiteSpace(x)
+                && !string.IsNullOrWhiteSpace(y)
+                && !string.IsNullOrWhiteSpace(z)
+                && y.IsValidDouble()
+                && z.IsValidDouble()));
+        }
+
+
+
+        public ReactiveCommand Save => _save;
+
+        public string PageTitle => _workUnit == null ? "Add Work" : "Edit Work";
+
+        public string Name
+        {
+            get { return _name; }
+            set { this.RaiseAndSetIfChanged(ref _name, value); }
+        }
+
+        public string Minutes
+        {
+            get { return _minutes; }
+            set { this.RaiseAndSetIfChanged(ref _minutes, value); }
+        }
+
+        public string Charge
+        {
+            get { return _charge; }
+            set { this.RaiseAndSetIfChanged(ref _charge, value); }
+        }
+
+
+
+        private IObservable<Unit> SaveImpl() => Observable.FromAsync(async () =>
+        {
+            var workUnit = new WorkUnit
+            {
+                Product = _product,
+                Name = Name,
+                Minutes = int.Parse(Minutes),
+                Charge = double.Parse(Charge) / 60
+            };
+            if (_workUnit != null) workUnit.Id = _workUnit.Id;
+
+            DBService.Write(realm => realm.Add(workUnit, true));
+
+            await NavigationService.GoBack();
+        });
+
+
+
+        private readonly ReactiveCommand<Unit, Unit> _save;
+        private string _name;
+        private string _minutes;
+        private string _charge;
+        private Product _product;
+        private WorkUnit _workUnit;
+    }
+}
--- StitchCalc/StitchCalc/Views/WorkUnitFormView.xaml.cs
+++ StitchCalc/StitchCalc/Views/WorkUnitFormView.xaml.cs
@@ -8,25 +8,26 @@ using StitchCalc.ViewModels;
 
 namespace StitchCalc.Views
 {
-	public partial class WorkUnitFormView : ReactiveContentPage<WorkUnitFormViewViewModel>
-	{
-		public WorkUnitFormView()
-		{
-			InitializeComponent();
+    public partial class WorkUnitFormView : ReactiveContentPage<WorkUnitFormViewModel>
+    {
+        public WorkUnitFormView(WorkUnitFormViewModel viewModel)
+        {
+            InitializeComponent();
 
-			ViewModel = new WorkUnitFormViewViewModel();
+            ViewModel = viewModel;
 
-			this.WhenActivated(disposables => {
-				this.OneWayBind(ViewModel, vm => vm.PageTitle, v => v.Title).DisposeWith(disposables);
-				this.Bind(ViewModel, vm => vm.Name, v => v.NameEntry.Text).DisposeWith(disposables);
-				this.Bind(ViewModel, vm => vm.Minutes, v => v.MinutesEntry.Text).DisposeWith(disposables);
-				this.Bind(ViewModel, vm => vm.Charge, v => v.ChargeEntry.Text).DisposeWith(disposables);
+            this.WhenActivated(disposables =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.PageTitle, v => v.Title).DisposeWith(disposables);
+                this.Bind(ViewModel, vm => vm.Name, v => v.NameEntry.Text).DisposeWith(disposables);
+                this.Bind(ViewModel, vm => vm.Minutes, v => v.MinutesEntry.Text).DisposeWith(disposables);
+                this.Bind(ViewModel, vm => vm.Charge, v => v.ChargeEntry.Text).DisposeWith(disposables);
 
-				this.BindCommand(ViewModel, vm => vm.Save, v => v.SaveToolbarItem).DisposeWith(disposables);
-				this.BindCommand(ViewModel, vm => vm.Save, v => v.NameEntry, nameof(Entry.Completed)).DisposeWith(disposables);
-				this.BindCommand(ViewModel, vm => vm.Save, v => v.MinutesEntry, nameof(Entry.Completed)).DisposeWith(disposables);
-				this.BindCommand(ViewModel, vm => vm.Save, v => v.ChargeEntry, nameof(Entry.Completed)).DisposeWith(disposables);
-			});
-		}
-	}
+                this.BindCommand(ViewModel, vm => vm.Save, v => v.SaveToolbarItem).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.Save, v => v.NameEntry, nameof(Entry.Completed)).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.Save, v => v.MinutesEntry, nameof(Entry.Completed)).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.Save, v => v.ChargeEntry, nameof(Entry.Completed)).DisposeWith(disposables);
+            });
+        }
+    }
 }
