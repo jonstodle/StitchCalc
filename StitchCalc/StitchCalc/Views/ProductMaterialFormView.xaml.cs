8beec925511fb4785348ceb8395f0bcda7db8a67
Unknown <jonstodle@outlook.com>
2017-02-27 13:58:41 +0100

    Fix material picker not setting correct selected index

--- StitchCalc/StitchCalc/Views/ProductMaterialFormView.xaml.cs
+++ StitchCalc/StitchCalc/Views/ProductMaterialFormView.xaml.cs
@@ -21,28 +21,16 @@ namespace StitchCalc.Views
 
             this.WhenActivated(disposables =>
             {
-                this.OneWayBind(ViewModel, vm => vm.PageTitle, v => v.Title).DisposeWith(disposables);
+                MaterialPicker.Items.Clear();
+				foreach (var material in ViewModel.Materials) MaterialPicker.Items.Add(material.Name);
+
+				this.OneWayBind(ViewModel, vm => vm.PageTitle, v => v.Title).DisposeWith(disposables);
                 this.Bind(ViewModel, vm => vm.SelectedMaterialIndex, v => v.MaterialPicker.SelectedIndex).DisposeWith(disposables);
                 this.Bind(ViewModel, vm => vm.Amount, v => v.AmountEntry.Text).DisposeWith(disposables);
 
                 this.BindCommand(ViewModel, vm => vm.Save, v => v.SaveToolbarItem).DisposeWith(disposables);
                 this.BindCommand(ViewModel, vm => vm.AddMaterial, v => v.AddMaterialButton).DisposeWith(disposables);
                 this.BindCommand(ViewModel, vm => vm.Save, v => v.AmountEntry, nameof(Entry.Completed)).DisposeWith(disposables);
-
-                ViewModel
-                    .Materials
-                    .CollectionChanges()
-                    .Select(_ => ViewModel.Materials)
-                    .StartWith(ViewModel.Materials)
-                    .Subscribe(items =>
-                    {
-                        MaterialPicker.Items.Clear();
-                        foreach (var item in items)
-                        {
-                            MaterialPicker.Items.Add(item.Name);
-                        }
-                    })
-                    .DisposeWith(disposables);
             });
         }
     }
