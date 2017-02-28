8499495c0e23428f9018adf1b6433a5c199d7bcb
Unknown <jonstodle@outlook.com>
2017-02-28 02:54:44 +0100

    Re-enable deleting of Material

--- StitchCalc/StitchCalc/ViewModels/MaterialViewModel.cs
+++ StitchCalc/StitchCalc/ViewModels/MaterialViewModel.cs
@@ -15,17 +15,22 @@ namespace StitchCalc.ViewModels
 			_material = material;
 
             _edit = ReactiveCommand.CreateFromObservable(() => Observable.FromAsync(() => NavigationService.NavigateTo(new MaterialFormView(new MaterialFormViewModel(_material)))));
+
+			_delete = ReactiveCommand.Create(() => DBService.Write(realm => realm.Remove(_material)));
 		}
 
 
 
         public ReactiveCommand Edit => _edit;
 
+		public ReactiveCommand Delete => _delete;
+
 		public Material Material => _material;
 
 
 
-        private readonly ReactiveCommand<Unit, Unit> _edit;
+		private readonly ReactiveCommand<Unit, Unit> _edit;
+        private readonly ReactiveCommand<Unit, Unit> _delete;
 		private readonly Material _material;
     }
 }
